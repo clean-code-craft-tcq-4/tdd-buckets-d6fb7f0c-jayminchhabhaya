b5726a508b76c37182f2d803fc5600e24a6b50e4
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-26 14:30:18 +0530

    Add return value

--- buckets.cpp
+++ buckets.cpp
@@ -37,6 +37,6 @@ std::vector<std::string> GetRanges(std::vector<int> samples)
         }
 	}
 	OutputMap.clear();
-	cout<<returnrangeList[0];
+	std::cout<<returnrangeList[0];
 	return returnrangeList;
 }
