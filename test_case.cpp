8684c0db88bc2463c4144ec9e3f0638ca1455df1
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-26 14:45:34 +0530

    Add return value

--- test_case.cpp
+++ test_case.cpp
@@ -4,9 +4,9 @@
 #include "buckets.hpp"
 
 TEST_CASE("TEST_CASE 2") {
-  std::vector<int> samples{4,5,6};
+  std::vector<int> samples{3, 3, 5, 4, 10, 11, 12};
   REQUIRE(detect_ranges(samples) == 1);
-  //auto objectofranges = GetRanges(samples);
+  auto objectofranges = GetRanges(samples);
   //REQUIRE(objectofranges.size() == 1);
-  //REQUIRE(objectofranges[0] == "4-6, 3");
+  REQUIRE(objectofranges[0] == "3-5, 4");
 }
