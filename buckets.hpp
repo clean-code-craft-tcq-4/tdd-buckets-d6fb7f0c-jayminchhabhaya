135e29197efa20addee10be05413200b77efffeb
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-26 15:05:22 +0530

    Add return value

--- buckets.hpp
+++ buckets.hpp
@@ -7,9 +7,3 @@
 int detect_ranges(std::vector<int> samples);
 void print_Output();
 std::vector<std::string> GetRanges(std::vector<int> samples);
-
-class MatadataRange
-{
-	public:
-		std::vector<std::string> m_strrange;
-};
--- test_case.cpp
+++ test_case.cpp
@@ -3,11 +3,19 @@
 #include "test/catch.hpp"
 #include "buckets.hpp"
 
-TEST_CASE("TEST_CASE 2") {
+TEST_CASE("TEST_CASE 1") {
   std::vector<int> samples{3, 3, 5, 4, 10, 11, 12};
   REQUIRE(detect_ranges(samples) == 1);
   auto objectofranges = GetRanges(samples);
   REQUIRE(objectofranges.size() == 2);
   REQUIRE(objectofranges[0] == "10-12, 3");
   REQUIRE(objectofranges[1] == "3-5, 4");
+}
+
+TEST_CASE("TEST_CASE 2") {
+  std::vector<int> samples{1,2,4,3};
+  REQUIRE(detect_ranges(samples) == 1);
+  auto objectofranges = GetRanges(samples);
+  REQUIRE(objectofranges.size() == 1);
+  REQUIRE(objectofranges[0] == "1-4, 4");
 }
