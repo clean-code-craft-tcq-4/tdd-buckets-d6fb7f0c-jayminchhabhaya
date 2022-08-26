ac290d17a776867b290676e4275fa2d14e4d60ff
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-26 15:01:01 +0530

    Add return value

--- test_case.cpp
+++ test_case.cpp
@@ -7,7 +7,7 @@ TEST_CASE("TEST_CASE 2") {
   std::vector<int> samples{3, 3, 5, 4, 10, 11, 12};
   REQUIRE(detect_ranges(samples) == 1);
   auto objectofranges = GetRanges(samples);
-  //REQUIRE(objectofranges.size() == 1);
+  REQUIRE(objectofranges.size() == 2);
   REQUIRE(objectofranges[0] == "10-12, 3");
   REQUIRE(objectofranges[1] == "3-5, 4");
 }
