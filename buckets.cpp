5ff9242c324bcca618d02828c1866ec4a079e0d6
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-26 16:02:12 +0530

    Add return value

--- buckets.cpp
+++ buckets.cpp
@@ -13,7 +13,7 @@ std::map<std::string, int> detect_ranges(std::vector<int> samples){
     std::string str="";
 	std::map<std::string, int>OutputMap{};
     for (int i = 0; i < samples.size(); i++){
-		std::count<<samples[i];
+		std::cout<<samples[i];
         if(samples[i]+1 >= (samples[i+1])){
             if(count == 1){
              str =std::to_string(samples[i]);   
@@ -23,7 +23,7 @@ std::map<std::string, int> detect_ranges(std::vector<int> samples){
         else{
            str +="-" + std::to_string(samples[i]);
            OutputMap[str] =  count;
-		   std::count<<str;
+		   std::cout<<str;
            count=1;
         }
 	}
