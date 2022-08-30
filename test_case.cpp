d928700e9e21dd0cc34247c404f8d2598ed3ec91
APAC\CJA2KOR <cja2kor@bosch.com>
2022-08-30 16:23:47 +0530

    Add A2D code

--- test_case.cpp
+++ test_case.cpp
@@ -41,3 +41,31 @@ TEST_CASE("TEST_CASE 5") {
 	REQUIRE(numberValue == 1146);
 	REQUIRE(A2DConvert(numberValue) == 3);
 }
+
+TEST_CASE("TEST_CASE 6") {
+    int BinaryValues[] = {0,1,0,0,0, 0,0,0,0, 0,0,0};
+    int numberValue = binary_to_number(BinaryValues,12);
+	REQUIRE(numberValue == 1024);
+	REQUIRE(A2DConvert(numberValue) == 3);
+}
+
+TEST_CASE("TEST_CASE 7") {
+    int BinaryValues[] = {1,0,0,1,1, 1,0,1,1, 1,0,1};
+    int numberValue = binary_to_number(BinaryValues,12);
+	REQUIRE(numberValue == 2525);
+	REQUIRE(A2DConvert(numberValue) == 6);
+}
+
+TEST_CASE("TEST_CASE 8") {
+    int BinaryValues[] = {0,0,0,0,0, 0,0,0,0, 0,0,0};
+    int numberValue = binary_to_number(BinaryValues,12);
+	REQUIRE(numberValue == 0);
+	REQUIRE(A2DConvert(numberValue) == 0);
+}
+
+TEST_CASE("TEST_CASE 9") {
+    int BinaryValues[] = {1,1,1,1,1, 1,1,1,1, 1,1,1};
+    int numberValue = binary_to_number(BinaryValues,12);
+	REQUIRE(numberValue == 4095);
+	REQUIRE(A2DConvert(numberValue) == 10);
+}
